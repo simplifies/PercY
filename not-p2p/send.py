015c6fee6bd48c2b58cebf2991072b916a1a427c
Echo <66488397+NullByte75@users.noreply.github.com>
2021-08-02 14:38:42 +0200

    Update send.py

--- not-p2p/send.py
+++ not-p2p/send.py
@@ -1,35 +1,108 @@
-import socket
-import os
-import sys
+from cryptography.fernet import Fernet
+import requests, json, os
 
-usercontact = "youruser2@percy.com"
+def generate_key():
+    key = Fernet.generate_key()
+    with open("storage/key.key", "wb") as f:
+        f.write(key)
+    return key
 
-default_server = "127.0.0.1"
-default_server_port = 1884
-contacts = {
-    "youruser@percy.com" #add as many contacts as you want
-}
-
-user = os.getenv("USERNAME")
-
-def client(ip, contact):
-
-    socks = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    try:
-        socks.connect((ip, default_server_port))
-    except:
-        print("Couldn't connect")
+def create_user(user, key):
+    data = {
+        "user": user, 
+        "key": key
+    }
+    rq = requests.post("http://127.0.0.1:5000/api/create_user", headers=data)
+    if rq.status_code == 200:
+        print("Account successfully created!")
+    if rq.status_code == 409:
+        print("Account already exists")
         exit()
-    firstrequest = socks.recv(1024).decode('ascii')
-    if firstrequest == "USER":
-        socks.send(usercontact.encode('ascii'))
-    socks.send("send".encode('ascii'))
-    message = input(">>> ")
-    socks.send(message.encode('ascii'))
-    socks.send(contact.encode('ascii'))
 
-if sys.argv[1] in contacts:
-  client(default_server, sys.argv[1])
-else:
-  print("Contact not found")
+def get_conversations(user,key):
+    data = {
+        "user": user, 
+        "key": key
+    }
+    rq = requests.get("http://127.0.0.1:5000/api/get_conversations", headers=data)
+    convs = json.loads(rq.text)
+    print("---- CONVERSATIONS ----")
+    for i in convs:
+        print(i)
+    print("\m")
+
+def get_messages(user,key,conversation):
+    data = {
+        "user": user, 
+        "key": key,
+        "conversation": conversation
+    }
+    rq = requests.get("http://127.0.0.1:5000/api/get_messages", headers=data)
+    data = json.loads(rq.text)
+    print("---- MESSAGES ----")
+    for i in data:
+        print(i)
+    print("\n")
+
+def send_messages(to,fromm,key,message):
+    data = {
+        "message_to": to,
+        "message_from": fromm,
+        "key": key,
+        "message": message
+    }
+    rq = requests.post("http://127.0.0.1:5000/api/send_message", headers=data)
+    if rq.status_code == 200:
+        print("Message sent!")
+
+def main():
+    help_list = """list: List all conversations
+    send_message: send a message to someone
+    get_messages: get messages
+    exit: exit percy
+    """
+    if not os.path.isfile("storage/user.txt"):
+        print(os.path.isfile("storage/user.txt"))
+        key = generate_key()
+        print("Enter username")
+        user = input(">>> ")
+        with open("storage/user.txt", "w") as f:
+            f.write(user)
+        create_user(user, key)
+        print(help_list)
+        while True:
+            inp = input(">>> ")
+            if inp == "list":
+                get_conversations(user, key)
+            if inp.startswith("send_message"):
+                to = input("Enter target user: ")
+                message = input("Enter message: ")
+                send_messages(to,user,key,message)
+            if inp.startswith("get_messages"):
+                conv = input("Enter conversation: ")
+                get_messages(user,key,conv)
+            if inp.startswith("exit"):
+                print("Exiting...")
+                exit()
+    else:
+        print(help_list)
+        with open("storage/key.key", "r") as f:
+            key = f.read()
+        with open("storage/user.txt", "r") as f:
+            user = f.read()
+        while True:
+            inp = input(">>> ")
+            if inp == "list":
+                get_conversations(user, key)
+            if inp.startswith("send_message"):
+                to = input("Enter target user: ")
+                message = input("Enter message: ")
+                send_messages(to,user,key,message)
+            if inp.startswith("get_messages"):
+                conv = input("Enter conversation: ")
+                get_messages(user,key,conv)
+            if inp.startswith("exit"):
+                print("Exiting...")
+                exit()
+
+main()
