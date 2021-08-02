a10592c7d99b08a50278f04709c18027679e2597
Echo <66488397+NullByte75@users.noreply.github.com>
2021-08-02 14:38:22 +0200

    Update server.py

--- not-p2p/server.py
+++ not-p2p/server.py
@@ -1,29 +1,76 @@
-import socket
-import threading
+from flask import Flask, render_template, request, abort, jsonify
+import os, json
 
-# handle client
-def handler(client_soc):
-    client_soc.send("USER".encode('ascii'))
-    user = client_soc.recv(1024).decode('ascii')
-    firstresponse = client_soc.recv(1024).decode('ascii')
-    if firstresponse == "send":
-        secondresponse = client_soc.recv(1024).decode('ascii')
-        contact = client_soc.recv(1024).decode('ascii')
-        with open(contact + ".txt", "a") as f:
-            f.write(user + ": " + secondresponse + "\n") # write message to contact file not the best way but for now it's ok
-        client_soc.close()
-    if firstresponse == "recieve":
-        with open(user + ".txt", "r") as f:
-            messages = f.read() # read messages
-        client_soc.send(messages.encode('ascii'))
-        client_soc.close()
-        with open(user + ".txt", "w") as f2:
-            f2.write("")
+app = Flask(__name__)
 
-with socket.socket() as socks:
-    socks.bind(('0.0.0.0', 1884))
-    socks.listen()
-    while True:
-        client_soc, client_address = socks.accept()
-        # create thread to handle client
-        threading.Thread(target=handler,args=(client_soc,), daemon=True).start() 
+@app.route("/api/create_user", methods=["POST"])
+def create_user():
+    if request.method == "POST":
+        data = request.headers
+        username = data["user"]
+        fernet_key = data["key"]
+        if os.path.isdir("users/" + username):
+            return "409", 409
+        os.mkdir("users/" + username)
+        os.mkdir("users/" + username + "/messages")
+        with open("users/" + username + "/key.txt", "w") as f:
+            f.write(fernet_key)
+        return "200", 200
+
+@app.route("/api/send_message", methods=["POST"])
+def send_message():
+    if request.method == "POST":
+        data = request.headers
+        message = data["message"]
+        message_from = data["message_from"]
+        fernet_key = data["key"]
+        message_to = data["message_to"]
+        with open("users/" + message_from + "/key.txt") as f:
+            key = f.read()
+        if fernet_key == key:
+            if not os.path.isdir("users/" + message_to):
+                abort(404)
+            else:
+                with open("users/" + message_from + "/messages/" + message_to + ".txt", "w") as f:
+                    f.write(message_from + ": " + message)
+                with open("users/" + message_to + "/messages/" + message_from + ".txt", "w") as f:
+                    f.write(message_from + ": " + message)
+                return "200", 200
+        else:
+            abort(401)
+
+@app.route("/api/get_conversations", methods=["GET"])
+def get_conversations():
+    if request.method == "GET":
+        data = request.headers
+        username = data["user"]
+        fernet_key = data["key"]
+        with open("users/" + username + "/key.txt") as f:
+            key = f.read()
+        if fernet_key == key:
+            files = os.listdir("users/" + username + "/messages")
+            return jsonify(files)
+        else:
+            abort(401)
+
+@app.route("/api/get_messages", methods=["GET"])
+def get_messages():
+    if request.method == "GET":
+        data = request.headers
+        username = data["user"]
+        fernet_key = data["key"]
+        conversation = data["conversation"]
+        with open("users/" + username + "/key.txt") as f:
+            key = f.read()
+        if fernet_key == key:
+            if not os.path.isfile("users/" + username + "/messages/" + conversation):
+                abort(404)
+            else:
+                with open("users/" + username + "/messages/" + conversation, "r") as f:
+                    messages = f.read()
+                    messages = messages.split("\n")
+                    return jsonify(messages)
+        else:
+            abort(401)
+
+app.run()
